b26f50dfd30a90b65a2bc8e588737ab3c9194865
Lauren Benichou <laurenbenichou@gmail.com>
2015-01-14 12:20:57 -0800

    remove line

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,5 @@ module TheLitographyProject
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
   end
 end
