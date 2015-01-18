e2e2bb03eca996421d0498d0df09180109359d34
Lauren Benichou <laurenbenichou@gmail.com>
2015-01-17 21:10:05 -0800

    fixing storage issue

--- app/admin/asset.rb
+++ app/admin/asset.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register Asset do
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+ permit_params :storage, :retained_storage, :remove_storage, :storage_url
+ index do
+    id_column
+    selectable_column
+    column :storage_name
+    column('Thumbnail') do |asset|
+        image_tag(asset.thumb_url)
+      end
+    actions
+  end
 
 end
