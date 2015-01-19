c7a70fe7d6c6c6077d356d26c2e871c6f4c1d35f
Lauren Benichou <laurenbenichou@gmail.com>
2015-01-19 13:43:44 -0800

    testing

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,16 +1,16 @@
 
-require 'dragonfly/rails/images'
+# require 'dragonfly/rails/images'
 
-app = Dragonfly[:images]
+# app = Dragonfly[:images]
 
-app.datastore = Dragonfly::DataStorage::S3DataStore.new
+# app.datastore = Dragonfly::DataStorage::S3DataStore.new
 
-app.datastore.configure do |c|
-  c.bucket_name = ENV['S3_BUCKET_NAME']
-  c.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  c.region = 'us-west-2'                        # defaults to 'us-east-1'
-  c.url_scheme = 'https'                        # defaults to 'http'
-end
+# app.datastore.configure do |c|
+#   c.bucket_name = ENV['S3_BUCKET_NAME']
+#   c.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+#   c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+#   c.region = 'us-west-2'                        # defaults to 'us-east-1'
+#   c.url_scheme = 'https'                        # defaults to 'http'
+# end
 
-print "running init"
+# print "running init"
