c223f4d6d5fb88e4dae0106abc05c302b7b0e783
Lauren Benichou <laurenbenichou@gmail.com>
2015-01-19 13:33:39 -0800

    re-installed gem

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,19 +1,16 @@
-# app = Dragonfly[:theLitographyProject]
-# app.datastore = Dragonfly::DataStorage::S3DataStore.new
-# app.datastore.configure do |c|
-#   c.bucket_name =
-#   c.access_key_id =
-#   c.secret_access_key =
-#   c.region = 'us-west-2'                        # defaults to 'us-east-1'
-#   c.url_scheme = 'https'                        # defaults to 'http'
-# end
 
 require 'dragonfly/rails/images'
 
 app = Dragonfly[:images]
-app.datastore = Dragonfly::DataStorage::S3DataStore.new({
-  :bucket_name        => ENV['S3_BUCKET_NAME'],
-  :access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-  :region => "us-west-2"
-})
+
+app.datastore = Dragonfly::DataStorage::S3DataStore.new
+
+app.datastore.configure do |c|
+  c.bucket_name = ENV['S3_BUCKET_NAME']
+  c.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  c.region = 'us-west-2'                        # defaults to 'us-east-1'
+  c.url_scheme = 'https'                        # defaults to 'http'
+end
+
+print "running init"
