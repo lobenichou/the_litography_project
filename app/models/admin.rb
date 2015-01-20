cda3cf7d06325194105204d5a4ebab7a5486f5b1
Lauren Benichou <laurenbenichou@gmail.com>
2015-01-19 19:43:05 -0800

    config admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,12 @@
 class Admin < ActiveRecord::Base
+  after_create { |admin| admin.send_reset_password_instructions }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def password_required?
+    new_record? ? false : super
+  end
+
 end
