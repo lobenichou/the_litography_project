3d28e793ef57ef26be987990451a86fe57eee0c9
Lauren Benichou <laurenbenichou@gmail.com>
2015-02-02 11:29:45 -0800

    player

--- app/assets/javascripts/angular/controllers/AuthorCtrl.js
+++ app/assets/javascripts/angular/controllers/AuthorCtrl.js
@@ -15,7 +15,7 @@ app.controller("AuthorCtrl", ['$scope', '$sce', '$modalInstance', 'showAuthor', 
                 );
 
     $scope.author = showAuthor
-    var htmlPlayer = '<iframe width="100%" height="120" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + $scope.author.book_report_link + '&amp;color=d9eded&amp;inverse=false&amp;auto_play=false&amp;show_user=false&amp;show_artwork=false"></iframe>'
+    var htmlPlayer = '<iframe width="100%" height="20" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + $scope.author.book_report_link + '&amp;color=d9eded&amp;inverse=false&amp;auto_play=false&amp;show_user=true"></iframe>'
     $scope.player =  $sce.trustAsHtml(htmlPlayer);
 }])
 
