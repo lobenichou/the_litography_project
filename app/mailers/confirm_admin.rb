8c06c304f13d1f14a4b205bc87ae936170821d36
Lauren Benichou <laurenbenichou@gmail.com>
2015-02-10 22:51:51 -0800

    modified email

--- app/mailers/confirm_admin.rb
+++ app/mailers/confirm_admin.rb
@@ -1,11 +1,11 @@
 class ConfirmAdmin < Devise::Mailer
-  default from: "lo@thelitographyproject.com"
+  default from: "ninna@thelitographyproject.com"
   helper :application # gives access to all helpers defined within `application_helper`.l`
   include Devise::Mailers::Helpers
 
   def new_user_waiting_for_approval(admin)
     @new_user = admin.email
     @url  = 'admins/sign_in'
-    mail(to: 'lo@thelitographyproject.com', subject: 'New User')
+    mail(to: 'ninna@thelitographyproject.com', subject: 'New User')
   end
 end
