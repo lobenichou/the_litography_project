7336d990d450e93c747936357522cfe665e6ba7f
Lauren Benichou <Lo@Laurens-iMac.local>
2015-03-18 20:16:37 -0700

    added bio

--- db/migrate/20150123070028_drop_active_admin_comments.rb
+++
@@ -1,5 +0,0 @@
-class DropActiveAdminComments < ActiveRecord::Migration
-  def change
-    drop_table :active_admin_comments
-  end
-end
--- db/migrate/20150123070756_drop_assets.rb
+++
@@ -1,5 +0,0 @@
-class DropAssets < ActiveRecord::Migration
-  def change
-    drop_table :assets
-  end
-end
